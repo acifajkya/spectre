a56bf89c9a1c217e824195e391fd1a898a0daf06
acifajkya <a.ceja4@csu.fullerton.edu>
2022-07-08 07:07:52 -0700

    Fixed Types

--- tests/Unit/Evolution/Systems/ScalarWave/Test_MomentumDensity.cpp
+++ tests/Unit/Evolution/Systems/ScalarWave/Test_MomentumDensity.cpp
@@ -26,8 +26,8 @@ void test_compute_item_in_databox(const DataType& used_for_size) {
   MAKE_GENERATOR(generator);
   std::uniform_real_distribution<> dist(-1., 1.);
 
-  const tnsr::i<DataVector, SpatialDim, Frame::Inertial> pi =
-      make_with_random_values<tnsr::i<DataVector, SpatialDim, Frame::Inertial>>(
+  const Scalar<DataVector> pi =
+      make_with_random_values<Scalar<DataVector>>(
           make_not_null(&generator), dist, used_for_size);
   const tnsr::i<DataVector, SpatialDim, Frame::Inertial> phi =
       make_with_random_values<tnsr::i<DataVector, SpatialDim, Frame::Inertial>>(
